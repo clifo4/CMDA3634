72c619bea1823d39d0be36589d4b27c6485d0665
Clifton <clifo4@vt.edu>
2018-02-20 16:58:26 -0500

    one more time

--- L11/mpi.c
+++ L11/mpi.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include<mpi.h>
 
 int main(int argc, char **argv) {
+  
+  MPI_Init(&argc, &argv);
+  int rank, size;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   //need running tallies
   long long int Ntotal;
   long long int Ncircle;
 
   //seed random number generator
-  double seed = 1.0;
+  double seed = rank;
   srand48(seed);
 
-  for (long long int n=0; n<1000000000;n++) {
+  for (long long int n=0; n<10000000;n++) {
     //gererate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
     double rand2 = drand48();
@@ -22,12 +29,23 @@ int main(int argc, char **argv) {
 
     //check if its in the circle
     if (sqrt(x*x+y*y)<=1) Ncircle++;
-    Ntotal++;
+    Ntotal++;/*
+    if(rank ==0 && n > 0 && n%100 == 0) {
+      printf("The current estimate for pi at step %ll is %f\n", n, 4*Ncircle/(double) Ntotal);
+    }*/
   }
 
   double pi = 4.0*Ncircle/ (double) Ntotal;
-
-  printf("Our estimate of pi is %f \n", pi);
-
+  double gPi;
+  MPI_Allreduce(&pi,
+                &gPi,
+                1,
+                MPI_DOUBLE,
+                MPI_SUM,
+                MPI_COMM_WORLD);
+  gPi /= (double)size;
+  printf("Rank %d estimate is %f\n", rank, pi);
+  printf("Our estimate of pi is %f \n", gPi);
+  MPI_Finalize();
   return 0;
 }
